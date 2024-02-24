3e816a849718f7977dfe4a3509f3391828b7571f
Anastasia <555.555.555.anastasie@gmail.com>
2024-02-24 18:13:18 +0300

    f

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+import createNextPWA from 'next-pwa';
+
+const withPWA = createNextPWA({
+  dest: 'public'
+})
+
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
